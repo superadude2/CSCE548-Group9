c863f59c5e83dcf8cbe4f8ef7518f5a12f2ea615
superadude2 <31374755+superadude2@users.noreply.github.com>
2018-02-11 12:25:03 -0500

    Added Printouts that will keep the user updated on what's happening

--- CSCE548_001_Project1/src/CSCE548_001_Project1.cpp
+++ CSCE548_001_Project1/src/CSCE548_001_Project1.cpp
@@ -27,10 +27,13 @@ int main(int argc, char** argv)
 
 	vector<string> dict;
 	ReadEnglishDictionary read_dict = ReadEnglishDictionary();
+	cout << "Getting Dictionary...";
 	dict = read_dict.getDictionary();
+	cout << "Completed." << endl;
 
 	string dict_2[dict.size()];
 
+	cout << "Preparing words in dictionary to be used as decryption keys.";
 	for( int i = 0; i < dict.size(); i++ )
 	{
 		dict_2[i] = dict.at(i);
@@ -40,16 +43,21 @@ int main(int argc, char** argv)
 		}
 		//cout << dict_2[i] << endl;
 	}
+	cout << "Completed." << endl;
 
 	string cipher_text = "8d20e5056a8d24d0462ce74e4904c1b513e10d1df4a2ef2ad4540fae1ca0aaf9";
 	//Target Plain Text = "This is a top secret." (No quotes)
 	vector<string> texts_to_decrypt;
 	vector<string> plain_texts;
+	cout << "Preparing ciphertexts and plaintexts...";
 	for( int i = 0; i < dict.size(); i++ )
 	{
 		texts_to_decrypt.push_back(cipher_text);
 		plain_texts.push_back("");
 	}
+	cout << "Completed." << endl;
+
+	cout << "Running Decryptions...";
 
 	//TODO Insert Decryption Code - Use each key in dict_2
 	//to decrypt one of the ciphertexts in texts_to_decrypt
@@ -57,7 +65,13 @@ int main(int argc, char** argv)
 	//The strings in plain_texts will be checked for valid words
 	//based on the dictionary.
 
+	cout << "Completed." << endl;
+
+	cout << "Checking for possible valid phrases...";
 	vector<string> good_texts = read_dict.checkForGoodPlainText(plain_texts);
+	cout << "Completed." << endl;
+	cout << endl;
+
 	cout << "Valid Possible Plain Texts:" << endl;
 	for( int i = 0; i < good_texts.size(); i++ )
 	{
